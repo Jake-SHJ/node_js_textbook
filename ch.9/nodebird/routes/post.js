de3304689531dc163025f4dbd78488e0d7126744
jake <guswns1853@gmail.com>
2021-02-27 22:30:26 +0900

    feat: Add following feature, hashtag feature / chapter end

--- ch.9/nodebird/routes/post.js
+++ ch.9/nodebird/routes/post.js
@@ -7,25 +7,26 @@ const { Post, Hashtag, User } = require("../models");
 const { isLoggedIn } = require("./middlewares");
 
 const router = express.Router();
-fs.readdir("uploads", (error) => {
-  if (error) {
-    console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
-    fs.mkdirSync("uploads");
-  }
-});
+try {
+  fs.readdir("uploads");
+} catch (error) {
+  console.error("uploads 폴더가 없어 uploads 폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
 
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename(req, file, cb) {
       const ext = path.extname(file.originalname);
-      cb(
-        null,
-        path.basename(file.originalname, ext) + new Date().valueOf() + ext
-      );
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
     },
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
 });
+
 router.post("/img", isLoggedIn, upload.single("img"), (req, res) => {
   console.log(req.file);
   res.json({ url: `/img/${req.file.filename}` });
@@ -54,6 +55,28 @@ router.post("/", isLoggedIn, upload2.none(), async (req, res, next) => {
   } catch (err) {
     console.error(err);
     next(err);
+  }
+});
+
+router.get("/hashtag", async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect("/");
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
+    }
+    return res.render("main", {
+      title: `${query} | NodeBird`,
+      user: req.user,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
 });
 module.exports = router;
