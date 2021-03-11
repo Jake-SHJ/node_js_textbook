4e1720865baf7ce28ae45521cb4ce660230b1768
jake <guswns1853@gmail.com>
2021-03-11 22:38:09 +0900

    feat: apply inquirer & chalk library

--- ch.14/node-cli/command.js
+++ ch.14/node-cli/command.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-const program = require("commander");
+const { program } = require("commander");
 const fs = require("fs");
 const path = require("path");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
 const htmlTemplate = `<!DOCTYPE html>
 <html>
@@ -59,40 +61,78 @@ const makeTemplate = (type, name, directory) => {
   if (type === "html") {
     const pathToFile = path.join(directory, `${name}.html`);
     if (exist(pathToFile)) {
-      console.error("이미 해당 파일이 존재합니다.");
+      console.error(chalk.bold.red("이미 해당 파일이 존재합니다."));
     } else {
       fs.writeFileSync(pathToFile, htmlTemplate);
-      console.log(pathToFile, "생성 완료");
+      console.log(chalk.green(pathToFile, "생성 완료"));
     }
   } else if (type === "express-router") {
     const pathToFile = path.join(directory, `${name}.js`);
     if (exist(pathToFile)) {
-      console.error("이미 해당 파일이 존재합니다.");
+      console.error(chalk.bold.red("이미 해당 파일이 존재합니다."));
     } else {
       fs.writeFileSync(pathToFile, routerTemplate);
-      console.log(pathToFile, "생성 완료");
+      console.log(chalk.green(pathToFile, "생성 완료"));
     }
   } else {
-    console.error("html 또는 express-router 둘 중 하나를 입력하세요.");
+    console.error(
+      chalk.bold.red("html 또는 express-router 둘 중 하나를 입력하세요.")
+    );
   }
 };
 
-program.version("0.0.1", "-v, --version").usage("[options]");
+program.version("0.0.1", "-v, --version").name("cli");
 
 program
   .command("template <type>")
-  .usage("--name <name> --path [path]")
+  .usage("<type> --filename [filename] --path [path]")
   .description("템플릿을 생성합니다.")
   .alias("tmpl")
-  .option("-n, --name <name>", "파일명을 입력하세요.", "index")
+  .option("-f, --filename [filename]", "파일명을 입력하세요.", "index")
   .option("-d, --directory [path]", "생성 경로를 입력하세요.", ".")
   .action((type, options) => {
     makeTemplate(type, options.name, options.directory);
   });
 
-program.command("*", { noHelp: true }).action(() => {
-  console.log("해당 명령어를 찾을 수 없습니다.");
-  program.help();
-});
-
-program.parse(process.argv);
+program
+  .action((cmd, args) => {
+    const hasArgs = args.args.length;
+    if (hasArgs) {
+      console.log(chalk.bold.red("해당 명령어를 찾을 수 없습니다."));
+      program.help();
+    } else {
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "type",
+            message: "템플릿 종류를 선택하세요.",
+            choices: ["html", "express-router"],
+          },
+          {
+            type: "input",
+            name: "name",
+            message: "파일의 이름을 입력하세요.",
+            default: "index",
+          },
+          {
+            type: "input",
+            name: "directory",
+            message: "파일이 위치할 폴더의 경로를 입력하세요.",
+            default: ".",
+          },
+          {
+            type: "confirm",
+            name: "confirm",
+            message: "생성하시겠습니까?",
+          },
+        ])
+        .then((answers) => {
+          if (answers.confirm) {
+            makeTemplate(answers.type, answers.name, answers.directory);
+            console.log(chalk.rgb(128, 128, 128)("터미널을 종료합니다."));
+          }
+        });
+    }
+  })
+  .parse(process.argv);
